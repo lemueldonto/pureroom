7f7c22e41e976499ada43e7dbac23921ab2a0b4f
José Ricardo Alves Figueirôa <jraf@cin.ufpe.br>
2022-02-03 18:18:28 +0100

    avatars

---
+++ src/app/components/avatar/avatar.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SeriesPoint }              from '../../interfaces/weather.interfaces';
+import { ReportService }            from '../../services/report.service';
+
+@Component({
+               selector:    'app-avatar',
+               templateUrl: './avatar.component.html',
+               styleUrls:   [ './avatar.component.css' ],
+           })
+export class AvatarComponent implements OnInit {
+
+    @Input('score') score: SeriesPoint = { time: Date.now(), value: 7 };
+
+    constructor(public reportService: ReportService) { }
+
+    ngOnInit(): void {
+    }
+
+}
